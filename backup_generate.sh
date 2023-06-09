4329562e5e5b76648754a9d65b3c8f9e47b722ac
sushank3 <sushankkr3@gmail.com>
2023-06-09 22:25:27 +0530

    backup_generate

---
+++ backup_generate.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+####################################
+# Author: Sushank Kumar
+# Date: 09-06-2023
+# Version: v1
+# This script will report the AWS usage.
+####################################
+
+# set -x
+
+# What to backup. 
+backup_files="/home/sushank/Code/Shell-Scripts"
+
+# Where to backup to.
+dest="/home/sushank/mount/backup"
+
+# Create archive filename.
+day=$(date +%A)                   
+hostname=$(hostname -s)
+#archive_file="$hostname-$day.tgz"
+archive_file="backup_`date +%d`_`date +%m`_`date +%Y`_`date +%A`.tar"
+
+# Print start status message.
+echo "Backing up $backup_files to $dest/$archive_file"
+date
+echo
+
+# Backup the files using tar.
+tar czf $dest/$archive_file $backup_files
+
+# Print end status message.
+echo
+echo "Backup finished"
+date
+
+# Long listing of files in $dest to check file sizes.
+ls -lh $dest

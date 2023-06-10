b8a2a24e6139bb951c67eb9e54ac0f7b993c4b16
sushank3 <sushankkr3@gmail.com>
2023-06-10 07:22:56 +0530

    update.sh

---
+++ update.sh
@@ -0,0 +1,17 @@
+sudo apt update -y && sudo apt upgrade -y
+
+# fix broken packages
+
+sudo apt --fix-missing update -y
+
+sudo apt update -y
+
+sudo apt install -f -y
+
+# Clean all the JUNK
+
+sudo apt clean
+
+sudo apt autoclean 
+
+sudo apt autoremove
